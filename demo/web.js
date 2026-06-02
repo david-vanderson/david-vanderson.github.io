5072b10ae43dee01f2e05c756ff5da053b2e4109
David Vanderson <david.vanderson@gmail.com>
2026-06-01 21:37:12 -0400

    may 2026, sync demo and docs

--- demo/web.js
+++ demo/web.js
@@ -430,7 +430,7 @@ export class Dvui {
             wasm_canvas_height: () => {
                 return this.gl.canvas.clientHeight;
             },
-            wasm_textureCreate: (pixels, width, height, interp) => {
+            wasm_textureCreate: (pixels, width, height, interp, wrap_u, wrap_v) => {
                 const pixelData = this.bytesFromPointer(pixels, width * height * 4);
 
                 const texture = this.gl.createTexture();
@@ -480,22 +480,22 @@ export class Dvui {
                         this.gl.LINEAR,
                     );
                 }
-                this.gl.texParameteri(
-                    this.gl.TEXTURE_2D,
-                    this.gl.TEXTURE_WRAP_S,
-                    this.gl.CLAMP_TO_EDGE,
-                );
-                this.gl.texParameteri(
-                    this.gl.TEXTURE_2D,
-                    this.gl.TEXTURE_WRAP_T,
-                    this.gl.CLAMP_TO_EDGE,
-                );
+                if (wrap_u === 1) {
+                    this.gl.texParameteri( this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
+                } else {
+                    this.gl.texParameteri( this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+                }
+                if (wrap_v === 1) {
+                    this.gl.texParameteri( this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
+                } else {
+                    this.gl.texParameteri( this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+                }
 
                 this.gl.bindTexture(this.gl.TEXTURE_2D, null);
 
                 return id;
             },
-            wasm_textureCreateTarget: (width, height, interp) => {
+            wasm_textureCreateTarget: (width, height, interp, wrap_u, wrap_v) => {
                 const texture = this.gl.createTexture();
                 const id = this.newTextureId;
                 //console.log("creating texture " + id);
@@ -539,16 +539,16 @@ export class Dvui {
                         this.gl.LINEAR,
                     );
                 }
-                this.gl.texParameteri(
-                    this.gl.TEXTURE_2D,
-                    this.gl.TEXTURE_WRAP_S,
-                    this.gl.CLAMP_TO_EDGE,
-                );
-                this.gl.texParameteri(
-                    this.gl.TEXTURE_2D,
-                    this.gl.TEXTURE_WRAP_T,
-                    this.gl.CLAMP_TO_EDGE,
-                );
+                if (wrap_u === 1) {
+                    this.gl.texParameteri( this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
+                } else {
+                    this.gl.texParameteri( this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+                }
+                if (wrap_v === 1) {
+                    this.gl.texParameteri( this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
+                } else {
+                    this.gl.texParameteri( this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+                }
 
                 this.gl.bindTexture(this.gl.TEXTURE_2D, null);
 
@@ -1301,6 +1301,7 @@ export class Dvui {
                     this.scroll_lowest_batch[0],
                 );
                 var ticks = -ev.deltaX;
+                var trackpad = 0;
                 if ((this.scroll_lowest_batch[0] >= 100) || // most wheels
                     (this.scroll_lowest_batch[0] === 16) || // mac firefox
                     (this.scroll_lowest_batch[0] === 9) || // mac firefox holding shift
@@ -1314,13 +1315,14 @@ export class Dvui {
                     //console.log("wheelX -deltaX " + -ev.deltaX + " ticks " + ticks);
                 } else {
                     // assume touchpad
+                    trackpad = 1;
                     ticks = ticks / this.scroll_lowest[0] * touchpad_adj;
                     //console.log("touchpadX -deltaX " + -ev.deltaX + " ticks " + ticks);
                 }
                 this.instance.exports.add_event(
                     4,
                     0,
-                    0,
+                    trackpad,
                     ticks,
                     0,
                 );
@@ -1336,6 +1338,7 @@ export class Dvui {
                     this.scroll_lowest_batch[1],
                 );
                 var ticks = -ev.deltaY;
+                var trackpad = 0;
                 if ((this.scroll_lowest_batch[1] >= 100) || // most wheels
                     (this.scroll_lowest_batch[1] === 16) || // mac firefox
                     (this.scroll_lowest_batch[1] === 4.000244140625)) { // mac safari/chrome
@@ -1347,13 +1350,14 @@ export class Dvui {
                     //console.log("wheelY -deltaY " + -ev.deltaY + " ticks " + ticks);
                 } else {
                     // assume touchpad
+                    trackpad = 1;
                     ticks = ticks / this.scroll_lowest[1] * touchpad_adj;
                     //console.log("touchpadY -deltaY " + -ev.deltaY + " ticks " + ticks);
                 }
                 this.instance.exports.add_event(
                     4,
                     1,
-                    0,
+                    trackpad,
                     ticks,
                     0,
                 );
